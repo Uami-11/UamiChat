cabc577b45fbbb0d65c59d5f21cc6e56e229b84e
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-15 10:08:05 +0545

    add: make the schema for the project

---
+++ server/models.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class User:
+    id: int
+    username: str
+    is_online: bool
+
+
+@dataclass
+class Room:
+    id: int
+    roomname: str
+    is_private: bool
+    owner_id: int
+
+
+@dataclass
+class Message:
+    id: int
+    sender_id: int
+    room_id: int | None
+    recipient_id: int | None
+    content: str
+    is_read: bool
+    created_at: datetime
