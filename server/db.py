3b10fa0653ddf0418202343c2ffb3372de2110d1
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-24 19:54:03 +0545

    add: make queries for messaging

--- server/db.py
+++ server/db.py
@@ -46,4 +46,69 @@ def create_user(username, hashed_password):
             id=row["id"], username=row["username"], is_online=row["is_online"]
         )
 
+
+def get_user_by_id(id):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute("SELECT * FROM users WHERE id=%s", (id,))
+
+    row = cur.fetchone()
+
+    if row:
+        return models.User(id=id, username=row["username"], is_online=row["is_online"])
     return None
+
+
+def set_user_online(user_id, status: bool):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(
+        "UPDATE users SET is_online=%s WHERE id=%s",
+        (
+            status,
+            user_id,
+        ),
+    )
+
+
+def save_message(sender_id, message, room_id=None, recipient_id=None):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(
+        "INSERT INTO messages (sender_id, message, room_id, recipient_id) VALUES (%s, %s, %s, %s)",
+        (sender_id, message, room_id, recipient_id),
+    )
+
+    row = cur.fetchone()
+    if row:
+        return row["message"]
+
+
+def get_unread_dms(user_id):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(
+        "SELECT m.sender_id, m.message FROM users u JOIN messages m ON m.recipient_id = u.id WHERE m.is_read = FALSE AND u.id = %s",
+        (user_id,),
+    )
+
+    unread_list = cur.fetchall()
+
+    return unread_list
+
+
+def mark_message_read(user_id, sender_id):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(
+        "UPDATE messages m JOIN users u ON m.recipient_id = u.id SET m.is_read = TRUE WHERE u.id = %s AND m.sender_id = %s",
+        (
+            user_id,
+            sender_id,
+        ),
+    )
