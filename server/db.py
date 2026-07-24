9f8785e8ac50cb0982b4f539d7541e0fb5bdb1d4
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-24 21:05:25 +0545

    add: put some types in the query function

--- server/db.py
+++ server/db.py
@@ -27,7 +27,7 @@ def get_user_by_username(username: str):
     return None
 
 
-def create_user(username, hashed_password):
+def create_user(username: str, hashed_password: str):
     conn = get_connection()
     cur = conn.cursor(cursor_factory=RealDictCursor)
 
