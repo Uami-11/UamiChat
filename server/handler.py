80e965b7db1d04a85409bdcc44fd6f7f19bb5208
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-28 12:06:19 +0545

    add: make functions to handle server responses

---
+++ server/handler.py
@@ -0,0 +1,86 @@
+import json
+
+import auth
+import db
+
+connected_clients = {}
+
+
+def register_client(user_id, websocket):
+    connected_clients[user_id] = websocket
+
+
+def unregister_client(user_id):
+    connected_clients.pop(user_id, None)
+    db.set_user_online(user_id, False)
+
+
+def send_to_client(user_id, message: dict):
+    websocket = connected_clients.get(user_id)
+    if websocket:
+        import asyncio
+        asyncio.ensure_future(websocket.send(json.dumps(message)))
+
+
+def broadcast_to_room(room_id, message: dict, exclude_user_id=None):
+    import db as database
+    cur = database.get_connection().cursor()
+    cur.execute("SELECT user_id FROM room_members WHERE room_id=%s", (room_id,))
+    members = cur.fetchall()
+    cur.connection.close()
+    for (user_id,) in members:
+        if user_id != exclude_user_id:
+            send_to_client(user_id, message)
+
+
+async def handle_message(websocket, raw_message: str):
+    data = json.loads(raw_message)
+    msg_type = data.get("type")
+
+    if msg_type == "register":
+        await handle_register(websocket, data)
+    elif msg_type == "login":
+        await handle_login(websocket, data)
+    elif msg_type == "ping":
+        await handle_ping(websocket, data)
+    else:
+        await websocket.send(json.dumps({
+            "type": "error",
+            "message": f"unknown message type: {msg_type}",
+        }))
+
+
+async def handle_register(websocket, data: dict):
+    result = auth.register_user(data["username"], data["password"])
+    if result["success"]:
+        await websocket.send(json.dumps({
+            "type": "success",
+            "message": "registered",
+            "username": result["user"].username,
+        }))
+    else:
+        await websocket.send(json.dumps({
+            "type": "error",
+            "message": result["message"],
+        }))
+
+
+async def handle_login(websocket, data: dict):
+    result = auth.login_user(data["username"], data["password"])
+    if result["success"]:
+        register_client(result["user"].id, websocket)
+        db.set_user_online(result["user"].id, True)
+        await websocket.send(json.dumps({
+            "type": "success",
+            "message": "logged in",
+            "username": result["user"].username,
+        }))
+    else:
+        await websocket.send(json.dumps({
+            "type": "error",
+            "message": result["message"],
+        }))
+
+
+async def handle_ping(websocket, data: dict):
+    await websocket.send(json.dumps({"type": "pong"}))
---
+++ server/main.py
@@ -0,0 +1,31 @@
+import asyncio
+import os
+
+import websockets
+
+import handler
+
+
+async def connection_handler(websocket):
+    try:
+        async for raw_message in websocket:
+            await handler.handle_message(websocket, raw_message)
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    finally:
+        for user_id, ws in list(handler.connected_clients.items()):
+            if ws is websocket:
+                handler.unregister_client(user_id)
+                break
+
+
+async def main():
+    host = os.environ.get("SERVER_HOST", "0.0.0.0")
+    port = int(os.environ.get("SERVER_PORT", "8765"))
+    async with websockets.serve(connection_handler, host, port):
+        print(f"UamiChat server running on ws://{host}:{port}")
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
